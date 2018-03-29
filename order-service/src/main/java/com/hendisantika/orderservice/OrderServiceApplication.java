38f611989951ff2b25f8871520e5a2c15d9ec337
Hendi Santika <hendisantika@gmail.com>
2018-03-30 06:08:54 +0700

    #22 - Enable Eureka CLient - order-service module

--- order-service/src/main/java/com/hendisantika/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/hendisantika/orderservice/OrderServiceApplication.java
@@ -1,12 +1,18 @@
 package com.hendisantika.orderservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+        LOGGER.debug("The order service is running .................");
+        SpringApplication.run(OrderServiceApplication.class, args);
 	}
 }
